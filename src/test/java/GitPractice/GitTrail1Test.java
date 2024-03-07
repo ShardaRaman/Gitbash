b89de3a90ca86d35a31226c78e4af20b38b1cfd2
ShardaRaman <shardaraman6@gmail.com>
2024-03-07 11:55:42 +0530

    committed

---
+++ src/test/java/GitPractice/GitTrail1Test.java
@@ -0,0 +1,15 @@
+package GitPractice;
+
+public class GitTrail1Test 
+{
+public void login() 
+{
+System.out.println("Enter your Username");	
+System.out.println("Enter your Password");
+System.out.println("login Successfull");
+}
+public void logOut()
+{
+	System.out.println("Logout successfull");
+}
+}
