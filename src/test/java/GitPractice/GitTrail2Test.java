475bb3532c06a182fb68e5235568d0a4bf7b00bb
ShardaRaman <shardaraman6@gmail.com>
2024-03-07 12:23:41 +0530

    message

---
+++ src/test/java/GitPractice/GitTrail2Test.java
@@ -0,0 +1,13 @@
+package GitPractice;
+
+public class GitTrail2Test 
+{
+public void takeAPicture()
+{
+	System.out.println("taken a picture successfully");
+}
+public void takeAScreenShot()
+{
+	System.out.println("taken a screenshot successfully");
+}
+}
